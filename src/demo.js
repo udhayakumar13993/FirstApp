20302f3f158cf00eadca51b7f362eba9ed6f3082
udhayakumar <mudhay93@gmail.com>
2020-09-09 08:10:33 +0530

    This is my first App

---
+++ src/demo.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+class Demo extends Component{
+
+    render(){
+
+        return(
+
+            <div>
+                <p>Demo App </p>    
+            </div>    
+        )
+    }
+}
+
+export default Demo;
